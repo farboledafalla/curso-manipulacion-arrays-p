bfed705eab84afc01d956baadd78b5e558bd5aef
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2023-12-12 19:21:11 -0500

    Se agrega método MAP

---
+++ server/02-map-exercise.js
@@ -0,0 +1,12 @@
+// Retornar un array multiplicado por 2 cada elemento
+
+const numeros = [2, 4, 6, 8];
+
+console.log(numeros.map((item) => item * 2));
+
+// const porDos = numeros.map((numItem) => {
+//    return numItem * 2;
+// });
+
+// console.log(numeros);
+// console.log(porDos);
